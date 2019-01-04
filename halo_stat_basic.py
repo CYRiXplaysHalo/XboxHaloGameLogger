3c9d78cb750aa4eb18a6f81ed3984c482ce882ec
CYRiXplaysHalo <43896332+CYRiXplaysHalo@users.noreply.github.com>
2019-01-03 23:17:40 -0500

    Remove offsets and address and put into separate file
    Remove offsets and address and put into separate file

--- halo_stat_basic.py
+++ halo_stat_basic.py
@@ -8,6 +8,7 @@ import time
 import socket
 import struct
 import datetime
+from memory_mappings_and_offsets import *
 
 #The test class is JayFoxRox's code.
 class Test(object):
@@ -99,18 +100,11 @@ def read_s16(address):
 def read_s32(address):
   return int.from_bytes(t.read(address, 4), 'little', signed=True)
 
-
-# datum array header fields (found by Xbox7887)
-datum_array_name_offset = 0x0
-datum_array_first_element_pointer_offset = 0x34
-datum_array_element_max_count_offset = 0x20
-datum_array_element_size_offset = 0x22
-
 # get player datum array info (found by Xbox7887)
 player_datum_array = read_u32(0x2FAD28)
-player_datum_array_max_count = read_u16(player_datum_array + datum_array_element_max_count_offset)
-player_datum_array_element_size = read_u16(player_datum_array + datum_array_element_size_offset)
-player_datum_array_first_element_address = read_u32(player_datum_array + datum_array_first_element_pointer_offset)
+player_datum_array_max_count = read_u16(player_datum_array + Datum_Array_Element_Max_Count_Offset)
+player_datum_array_element_size = read_u16(player_datum_array + Datum_Array_Element_Size_Offset)
+player_datum_array_first_element_address = read_u32(player_datum_array + Datum_Array_First_Element_Pointer_Offset)
 
 # function to get player's element date (found by Xbox7887)
 def get_players_element_data(player_index, offset, number_of_bytes):
@@ -128,18 +122,18 @@ player_stat_array = []
 # TODO clean up offsets by converting to hex
 for player_index in range(player_count):
   player_stats = {}
-  player_stats['kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +152)
-  player_stats['team_kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +168)
-  player_stats['deaths'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +170)
-  player_stats['suicides'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +172)
-  player_stats['name'] = t.read(player_datum_array_first_element_address + player_index * player_datum_array_element_size + 4,24).decode("utf-16").split('\x00',1)[0]
+  player_stats['kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Kills_Offset)
+  player_stats['team_kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Team_Kills_Offset)
+  player_stats['deaths'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Deaths_Offset)
+  player_stats['suicides'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Suicides_Offset)
+  player_stats['name'] = t.read(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Player_Name_Offset,24).decode("utf-16").split('\x00',1)[0]
   player_stats['index'] = player_index
   player_stat_array.append(player_stats)
 
 while True:
     
-  score1_new = read_s8(0x276710) # 0x276710 is location of red team score in team slayer games
-  score2_new = read_s8(0x276714) # 0x276714 is location of blue team score in team slayer games
+  score1_new = read_s8(Team_Slayer_Red_Score_Address) # 0x276710 is location of red team score in team slayer games
+  score2_new = read_s8(Team_Slayer_Blue_Score_Address) # 0x276714 is location of blue team score in team slayer games
 
   #if score changes
   if(score1 != score1_new or score2 != score2_new):
@@ -152,11 +146,11 @@ while True:
     player_stat_array_new = []
     for player_index in range(player_count):
       player_stats = {}
-      player_stats['kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +152)
-      player_stats['team_kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +168)
-      player_stats['deaths'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +170)
-      player_stats['suicides'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size +172)
-      player_stats['name'] = t.read(player_datum_array_first_element_address + player_index * player_datum_array_element_size + 4,24).decode("utf-16").split('\x00',1)[0]
+      player_stats['kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Kills_Offset)
+      player_stats['team_kills'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Team_Kills_Offset)
+      player_stats['deaths'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Deaths_Offset)
+      player_stats['suicides'] = read_s8(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Suicides_Offset)
+      player_stats['name'] = t.read(player_datum_array_first_element_address + player_index * player_datum_array_element_size + Player_Name_Offset,24).decode("utf-16").split('\x00',1)[0]
       player_stats['index'] = player_index
       player_stat_array_new.append(player_stats)
 
---
+++ memory_mappings_and_offsets.py
@@ -0,0 +1,38 @@
+#Static_Addresses (found by Xbox7887):
+Player_Datum_Array_Pointer = 0x2FAD28
+Object_Header_Datum_Array_Pointer = 0x2FC6AC
+Team_Slayer_Red_Score_Address = 0x276710
+Team_Slayer_Blue_Score_Address  = 0x276714
+
+#Datum_Array_Header_Fields (found by Xbox7887):
+Datum_Array_Name_Offset = 0x0
+Datum_Array_First_Element_Pointer_Offset = 0x34
+Datum_Array_Element_Max_Count_Offset = 0x20
+Datum_Array_Element_Size_Offset = 0x22
+
+
+#Player_Datum_Array_Value_Offsets:
+Player_Name_Offset = 0x4
+Kills_Offset = 0x98
+Team_Kills_Offset = 0xA8 #(Betrays_+_Suicides?)
+Deaths_Offset = 0xAA
+Suicides_Offset = 0xAC
+
+#Player_Object_Array_Value_Offsets (found with the help of Devieth):
+XCoord_Offset = 0xC #(float)
+YCoord_Offset = 0x10 #(float)
+ZCoord_Offset = 0x14 #(float)
+XVel_Offset = 0x18 #(float)
+YVel_Offset = 0x1C #(float)
+ZVel_Offset = 0x20 #(float)
+Pitch_Offset = 0x24 #(float)
+Yaw_Offset = 0x28 #(float)
+Roll_Offset = 0x2C #(float)
+XScale_Offset = 0x30 #(float)
+YScale_Offset = 0x34 #(float)
+ZScale_Offset = 0x38 #(float)
+Max_Health_Offset = 0x88 #(float,_default_value_is_75)
+Max_Shields_Offset = 0x8C #(float,_defaeult_value_is_1)
+Health_Percentage_Offset = 0x90 #(float)
+Shield_Percentage_Offset = 0x94 #(float)
+Time_Elapsed_Offset = 0xA #(Minutes,_possibly_while_player_is_only_alive?)
